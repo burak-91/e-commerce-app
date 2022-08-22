b6ef47c8c71de7c593c345273ed512f0e28e1a1a
burak-91 <burakeroksuz@gmail.com>
2022-08-22 14:12:32 +0300

    added main main menu links

--- src/components/Directory/DirectoryItem/DirectoryItem.jsx
+++ src/components/Directory/DirectoryItem/DirectoryItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import {DirectoryItemContainer, Body, BackgroundImage} from './directoryItem.style.js'
+import {useNavigate} from 'react-router-dom'
 
 const DirectoryItem = ({category}) => {
-  const {title, imageUrl} = category
+  const {title, imageUrl, route} = category
+  const navigate = useNavigate()
+
+  const handleMoveToPage = () => navigate(route)
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={handleMoveToPage}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
